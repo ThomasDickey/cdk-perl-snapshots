b421ebe2c3379b4e7b4e61aeafbced5b7a017469
Thomas E. Dickey <dickey@invisible-island.net>
2015-09-29 01:05:08 -0400

    snapshot of project "cdk-perl", label t20150928

--- Cdk.pm
+++ Cdk.pm
@@ -1,4 +1,4 @@
-# $Id: Cdk.pm,v 1.13 2014/11/07 01:33:19 tom Exp $
+# $Id: Cdk.pm,v 1.14 2015/09/29 01:04:41 tom Exp $
 
 package Cdk;
 
@@ -12,7 +12,7 @@ use AutoLoader ();
 select (STDIN); $| = 1 ;
 
 # Set the version.
-$VERSION = "5.20141106"; # must be a floating-point number
+$VERSION = "5.20150928"; # must be a floating-point number
 
 # Set the diag flag off.
 $DIAGFLAG = 0;
